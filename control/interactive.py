a918af2a6ee032f5d0e7870a7e80279e36d14465
Piotr Gorczyca <piotr.gorczyca@tu-dresden.de>
2025-05-05 21:33:08 +0200

    Interactive import aliases.

--- control/interactive.py
+++ control/interactive.py
@@ -1,4 +1,4 @@
-from clingo import Control, Number, Function
+from clingo import Control, Number as N, Function as F
 
 
 def run(instance, base, encoding):
@@ -14,13 +14,14 @@ def run(instance, base, encoding):
   print(f'Setting: {base}')   
   while True:
     print('Current dispute:\n'+'\n'.join(f"{i}. {e}" for i, e in enumerate(moves)))
-    ctl.ground([('updateState', [Number(step)])])    
-    p_win = Function('end', [Number(step), Function("p")])
+    ctl.ground([('updateState', [N(step)])])    
+    p_win = F('end', [N(step), F("p")])
     res = ctl.solve(assumptions=[(p_win, True)])
     if res.satisfiable:
         print('YES')
         return
-    o_win = Function('end', [Number(step), Function("o")])
+
+    o_win = F('end', [N(step), F("o")])
     res = ctl.solve(assumptions=[(o_win, False)])
     if res.unsatisfiable:
         print('NO')
@@ -32,12 +33,12 @@ def run(instance, base, encoding):
             pm_atoms = list(filter(lambda atom:
                 atom.name == 'pm' 
                 and len(atom.arguments) > 0
-                and atom.arguments[0] == Number(step-1), m.symbols(atoms=True)))
+                and atom.arguments[0] == N(step-1), m.symbols(atoms=True)))
             
             print('Possible moves:\n'+'\n'.join(f"{i}. {str(e)}" for i, e in enumerate(pm_atoms)))
             sel_move = pm_atoms[int(input(f"Select (0-{len(pm_atoms)-1}): "))]
-            move = Function('m', [Number(step), *sel_move.arguments[1:]])
+            move = F('m', [N(step), *sel_move.arguments[1:]])
             moves = [*moves, str(move)]
             
-    ctl.ground([('pmc', [Number(step), *move.arguments[1:]])])
+    ctl.ground([('pmc', [N(step), *move.arguments[1:]])])
     
